913bdce6b086aacd7b839d4c20458d1cba209020
Fernando Kain <fernandomanga@hotmail.com>
2021-12-16 01:20:54 -0300

    Acréscimo de método que exibe funcionários agrupados por tipo de contratação.

--- src/test/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/test/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 1000.0, "CLT")
+    val pedro = Funcionario("Pedro", 2000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(maria, joao, pedro)
 
@@ -15,9 +15,16 @@ fun main(){
     println(funcionarios.find { it.nome == "Maria" })
 
     println("----------------------------------------------")
+    println("Método que exibirá através de grupos. No caso:")
+    funcionarios
+        .sortedBy { it.tipoContracao }
+        .forEach{(println(it))}
+
+
+    println("----------------------------------------------")
     println("Método que ordenará a exibição dos funcionários pelo salário do menor para o maior")
     funcionarios
-        .sortedBy { it.salario }
+        .groupBy { it.salario }
         .forEach{(println(it))}
 
 
@@ -25,7 +32,8 @@ fun main(){
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContracao: String
 ) {
     // Função que sobrescreve a maneira de exibição. Método especial do Kotlin que deixa muito mais bonita a impressão.
     // Para visualizar o outro modo é só comentar o override.
@@ -33,6 +41,7 @@ data class Funcionario(
         """
             Nome: $nome
             Salário: $salario
+            Tipo de Contratação: $tipoContracao
         """.trimIndent()
     }
 
